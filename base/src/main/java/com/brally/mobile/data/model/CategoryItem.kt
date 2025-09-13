5ee7387977a732f99002fedcbd68cf4da9cd1be9
Im-Shocker <giacatdu1412@gmail.com>
2025-09-13 19:36:39 +0700

    update CategoryItem.kt

--- base/src/main/java/com/brally/mobile/data/model/CategoryItem.kt
+++ base/src/main/java/com/brally/mobile/data/model/CategoryItem.kt
@@ -1,6 +1,10 @@
 package com.brally.mobile.data.model
 
+import androidx.annotation.DrawableRes
+import com.brally.mobile.base.R
+
 data class CategoryItem(
     var type: String = "",
     var value: String = "",
+    @DrawableRes val iconResId: Int = R.drawable.img_placeholder
 )
